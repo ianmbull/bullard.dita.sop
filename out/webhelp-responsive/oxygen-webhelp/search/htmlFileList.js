a2157041e641d7100a08973c9b94ca8104a2fedd
ianmbull <ianmbull@vt.edu>
2016-10-16 23:44:39 -0400

    bullard dita sop

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,13 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "concept_materials_needed.html";
+fl["1"]= "concept_preparing_machine.html";
+fl["2"]= "taks_fresh_strawberry.html";
+fl["3"]= "task_cleaning.html";
+fl["4"]= "task_making_desserts.html";
+fl["5"]= "task_peanut_butter_cup.html";
+fl["6"]= "task_vanilla.html";
+fl["7"]= "task_warnings.html";
+fl["8"]= "topic_introduction.html";
+fl["9"]= "topic_troubleshooting.html";
+var doStem = false;searchLoaded = true;
